a8273fbb9eff53badb1ecdbd49faed9b8fdaf52b
sethastaydohar <seth.a.staydohar@gmail.com>
2025-11-06 12:29:40 -0600

    seth

--- MapApi/obj/Debug/net9.0/MapApi.AssemblyInfo.cs
+++ MapApi/obj/Debug/net9.0/MapApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MapApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a326d4523077819dbf62607800c1005c5273b6bc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+257525a595d16cc99d69460fd970772847718259")]
 [assembly: System.Reflection.AssemblyProductAttribute("MapApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MapApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
